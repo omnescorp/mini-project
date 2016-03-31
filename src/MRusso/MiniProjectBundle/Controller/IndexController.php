e7c76750962b97020dcfc12b3504f11909154346
Marco Russo <marco.russo@vassdigital.com>
2016-03-31 11:40:41 +0200

    Post view complete

--- src/MRusso/MiniProjectBundle/Controller/IndexController.php
+++ src/MRusso/MiniProjectBundle/Controller/IndexController.php
@@ -4,6 +4,8 @@ namespace MRusso\MiniProjectBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class IndexController extends Controller {
 
@@ -16,6 +18,15 @@ class IndexController extends Controller {
 
         return $this->render('MRussoMiniProjectBundle:index:index.html.twig', array(
                     'posts' => $this->get('post')->getAllFront(),
+        ));
+    }
+
+    public function viewAction(Request $request) {
+        if (!$post = $this->get('post')->find($request->get('id'))) {
+            throw new NotFoundHttpException('No post');
+        }
+        return $this->render('MRussoMiniProjectBundle:index:view.html.twig', array(
+                    'post' => $post
         ));
     }
 
