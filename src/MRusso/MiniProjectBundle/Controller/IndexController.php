ed1c8042c98f624cc08eb5307e340b50e3d5cf16
Marco Russo <marco.russo@vassdigital.com>
2016-03-31 13:18:35 +0200

    Cambios

--- src/MRusso/MiniProjectBundle/Controller/IndexController.php
+++ src/MRusso/MiniProjectBundle/Controller/IndexController.php
@@ -6,14 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-
-
 class IndexController extends Controller {
 
     public function indexAction(Request $request) {
         if ($request->getMethod() == 'POST' && $request->files->get('media_file')) {
-            $this->get('post')->create();
-            $this->addFlash('success', 'Post Created');
+            if ($this->get('post')->create()) {
+                $this->addFlash('success', 'Post Created');
+            } else {
+                $this->addFlash('error', 'An error occurred');
+            }
             return $this->redirectToRoute('_homepage');
         }
 
