307a4f3a8d51224e13d8459790497836362a83f3
Marco Russo <marco.russo@vassdigital.com>
2016-03-31 10:37:10 +0200

    folder for images

--- src/MRusso/LibBundle/Model/Post.php
+++ src/MRusso/LibBundle/Model/Post.php
@@ -9,7 +9,7 @@ use MRusso\LibBundle\Service\DB;
 class Post extends DB {
 
     protected $entity = 'MRusso\LibBundle\Entity\Post';
-    
+
     public function getAllFront() {
         $query = $this->em->createQueryBuilder();
 
@@ -22,6 +22,77 @@ class Post extends DB {
 //        echo $query->getQuery()->getSql();die;
 //	return $this->getOne($query);
         return $this->OrmPaginator($query);
+    }
+
+    public function create($post = null) {
+        $date = new \DateTime("now");
+//        echo get_class($date);die;
+
+        if (!$post) {
+            $post = new $this->entity;
+        }
+        $post
+                ->setPostTitle($this->request->get('post_title'))
+                ->setPostImage($this->request->get('post_title'))
+                ->setPostDate($date)
+//                ->setPostUser($this->container->get('mrusso.user')->getUser())
+
+
+        ;
+        $this->em->persist($post);
+        $this->em->flush();
+        return $post;
+    }
+
+    public function setFile(UploadedFile $file = null) {
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->path)) {
+            // store the old name to delete after the update
+            $this->temp = $this->path;
+            $this->path = null;
+        } else {
+            $this->path = $this->file->getClientOriginalName();
+        }
+    }
+
+    public function getFile() {
+        return $this->file;
+    }
+
+    public function upload() {
+        if ($this->request->files->get('media_file')) {
+            $files = array();
+            if (is_array($this->request->files->get('media_file'))) {
+                $files = $this->request->files->get('media_file');
+            } else {
+                $files[] = $this->request->files->get('media_file');
+            }
+            foreach ($files as $file) {
+
+                if (null === $file) {
+                    continue;
+                }
+                $this->setFile($file);
+                if ($target = $this->getFile()->move('bundles/mrussominiproject/img/', $this->path)) {
+
+                    $galerie = new $this->entity;
+                    $galerie->setGalerieFile($target);
+                    $this->em->persist($galerie);
+                    $this->em->flush();
+                    continue;
+                }
+
+                // check if we have an old image
+//            if (isset($this->temp)) {
+//                // delete the old image
+//                unlink($this->getUploadRootDir() . '/' . $this->temp);
+//                // clear the temp image path
+//                $this->temp = null;
+//            }
+                $this->file = null;
+            }
+        }
     }
 
 }
--- src/MRusso/MiniProjectBundle/Controller/IndexController.php
+++ src/MRusso/MiniProjectBundle/Controller/IndexController.php
@@ -3,10 +3,17 @@
 namespace MRusso\MiniProjectBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller {
 
-    public function indexAction() {
+    public function indexAction(Request $request) {
+        if ($request->getMethod() == 'POST' && $request->files->get('media_file')) {
+            $this->get('post')->create();
+            $this->addFlash('success', 'Post Created');
+//            return $this->redirectToRoute('MRussoGalerieBundle:Index:index');
+        }
+        
         return $this->render('MRussoMiniProjectBundle:index:index.html.twig', array(
                     'posts' => $this->get('post')->getAllFront(),
         ));
